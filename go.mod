20a717c3696d1125ce5fe05579b04870e55e0357
Aleksandr Baranov <a.n.baranov@outlook.com>
2023-12-17 13:19:46 +0300

    feat: server init/run, req/resp helpers, middleware added

--- go.mod
+++ go.mod
@@ -30,6 +30,7 @@ require (
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/muonsoft/language v0.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce
 	golang.org/x/sync v0.5.0
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ internal/server/error.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+	"runtime/debug"
+	"strings"
+
+	"github.com/Employee-s-file-cabinet/backend/internal/server/internal/api"
+	"github.com/Employee-s-file-cabinet/backend/internal/server/internal/response"
+)
+
+var (
+	ErrInvalidContentType     = errors.New("invalid content type")
+	ErrLimitRequestBodySize   = errors.New("request body too large")
+	ErrBadContentLengthHeader = errors.New("bad content length header: missing or not a number")
+	ErrInternalServerError    = errors.New("the server encountered a problem and could not process your request")
+)
+
+func (s *server) reportServerError(r *http.Request, err error, withStack bool) {
+	var (
+		message = err.Error()
+		method  = r.Method
+		url     = r.URL.String()
+		trace   = string(debug.Stack())
+	)
+
+	requestAttrs := slog.Group("request", "method", method, "url", url)
+	if withStack {
+		s.logger.Error(message, requestAttrs, "trace", trace)
+	} else {
+		s.logger.Error(message, requestAttrs)
+	}
+}
+
+func (s *server) errorMessage(w http.ResponseWriter, r *http.Request, status int, message string, headers http.Header) {
+	message = strings.ToUpper(message[:1]) + message[1:]
+
+	err := response.JSONWithHeaders(w, status, api.Error{Message: message}, headers)
+	if err != nil {
+		s.reportServerError(r, err, false)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
